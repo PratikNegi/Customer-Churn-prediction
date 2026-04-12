19cd1af33725e0072782fc6cfa34b2f35bb6ba67
PratikNegi <pratiknegi.r@gmail.com>
2026-04-12 19:32:20 +0530

    fixes the app

--- app.py
+++ app.py
@@ -12,31 +12,35 @@ import numpy as np
 
 scaler=joblib.load("scaler.pkl")
 model=joblib.load("model.pkl")
+oe = joblib.load("OrdinalEncoder.pkl")
 
 st.title("Customer Churn Prediction")
 st.divider()
-Age =st.number_input("Enter your age", min_value=10, max_value=100)
-Gender = st.selectbox("Select your gender", ["Male", "Female"])
-Tenure= st.number_input("Enter your Tenure")
-Monthly_Charges= st.number_input("Enter your Monthly_Charges", min_value=15, max_value=200,value= 40)
-contractType = st.selectbox("Select your contract ", ['Month-to-month', 'One year', 'Two year'])
-Total_Charges= st.number_input("Enter your TotalCharges", max_value=15000)
-TechSupport = st.selectbox("Select your TechSupport", ['Yes', 'No'])
-Internet = st.selectbox("Select your internet service", ['DSL', 'Fiber Optic', 'Unknown'])
+Age =st.number_input("Enter your Age",value=20, min_value=10, max_value=100)
+Gender = st.selectbox("Select your Gender", ["Male", "Female"])
+Tenure= st.number_input("Enter your Tenure", value =19)
+Monthly_Charges= st.number_input("Enter the Monthly Charges (in $)", min_value=15, max_value=200,value= 40)
+contractType = st.selectbox("Select your contract type", ['Month-to-Month', 'One-Year', 'Two-Year'])
+Total_Charges= Tenure * Monthly_Charges
+TechSupport = st.selectbox("TechSupport", ['Yes', 'No'])
+Internet = st.selectbox("Select your Internet service", ['DSL', 'Fiber Optic', 'Unknown'])
 st.divider()
 
 Internet_Service = [0,0,0]
 if Internet == 'DSL':
     Internet_Service[0] = 1 
-elif Internet == "Fiber opic":
+elif Internet == "Fiber optic":
     Internet_Service[1] = 1
 else:
     Internet_Service[2] = 1 
 
 prediction= st.button("Predict")
 if prediction:
+    contractType_encoded = oe.transform([[contractType]])[0][0]
+    print(contractType_encoded)
     gender = 1 if Gender=="Female" else 0
-    col = [Age, gender, Tenure, Monthly_Charges,contractType, Total_Charges, TechSupport] 
+    TechSupport= 1 if TechSupport=="Yes" else 0
+    col = [Age, gender, Tenure, Monthly_Charges, contractType_encoded, Total_Charges, TechSupport] 
     col.extend(Internet_Service)
     print(col)
     colArr = np.array(col)
@@ -46,7 +50,7 @@ if prediction:
     resShow = "Churn" if result==1 else  "Not Churn"
     st.success(resShow)
 else :
-    st.error("please enter the correct number and click on the predict button")
+    st.error("please enter the correct details and click on the predict button")
 
 
 
